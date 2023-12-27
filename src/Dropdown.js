0bd45da7b3bea2332c4926587b41d84ecf8722b6
Rayven <rayvenponce@gmail.com>
2023-12-27 11:00:50 +0800

    Fixed dropdown

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,18 +1,18 @@
 function Dropdown() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById('myDropdown').classList.toggle('show');
 }
 
-window.onclick = function(event) {
-    if (!event.target.matches('.droptbn')) {
-        var dropdowns = document.getElementsByClassName("dropdpwn-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if(openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        } 
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName('dropdown-content');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
-}
+  }
+};
 
-export default Dropdown
+export default Dropdown;
--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,30 @@
-import Dropdown from "./Dropdown.js";
-import './App.css';
-import { Outlet, Link } from "react-router-dom";
+// Header.js
+import React from 'react';
+import "./App.css";
+import Dropdown from './Dropdown';
+import { Outlet, Link } from 'react-router-dom';
 
 const Header = () => {
-    return (
-        <>
-            <div className='header'>
-                <div className='my-logo'>
-                    <img src="./M.png" className="m-logo" alt="logo" />
-                </div>
-                <div className='dropDown'>
-                    <button onClick={ Dropdown } className='dropbtn'>☰</button>
-                    <div id='myDropdown' className='dropdown-content'>
-                        <Link to='/Body'>Home</Link>
-                        <Link to='/BodyAbout'>About</Link>
-                        <Link to='/BodyContact'>Contact</Link>
-                    </div>
-                </div>
-            </div>
-            <Outlet />
-        </>
-    )
+  return (
+    <>
+      <div className='header'>
+        <div className='my-logo'>
+          <img src="./M.png" className="m-logo" alt="logo" />
+        </div>
+        <div className='dropDown'>
+          <button onClick={Dropdown} className='dropbtn'>
+            ☰
+          </button>
+          <div id='myDropdown' className='dropdown-content'>
+            <Link to='/Body'>Home</Link>
+            <Link to='/BodyAbout'>About</Link>
+            <Link to='/BodyContact'>Contact</Link>
+          </div>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
 };
 
 export default Header;
